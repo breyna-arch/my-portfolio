b3255055a09b262a48ae5e3acd16c3b8597d579e
brianreyna <brianreyna@csus.edu>
2025-12-06 14:27:34 -0800

    Updates

---
+++ assets/stateDiagram-v2-4FDKWEC3-CrZ1JZ1f.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-DhcFMPea.js";import{_ as i}from"./index-B9p7iDKX.js";import"./chunk-55IACEB6-BnDKynRm.js";import"./chunk-QN33PNHL-Bbou9TBT.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
--- src/pages/FoodBlog.jsx
+++
@@ -1,9 +0,0 @@
-export default function FoodBlog() {
-  return (
-    <div className="food-blog">
-      <h1>Welcome to my Food Blog!</h1>
-      <p>Here I share recipes, food adventures, and culinary experiments!</p>
-    </div>
-    
-  );
-}
